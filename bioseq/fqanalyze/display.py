66b8ede1b85bb401b502dc0a797195f478b1b0e1
Trey <ten.mermaido@gmail.com>
2023-01-11 07:40:35 +0700

    1101230740

--- bioseq/fqanalyze/display.py
+++ bioseq/fqanalyze/display.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import json
-import pandas as pd
 import numpy as np
 from collections import Counter
 from io import BytesIO
@@ -27,29 +26,154 @@ def sum_q (list_data):
     return result_value
 
 def group_list(lst):      
-    return list(zip(Counter(lst).keys(), Counter(lst).values()))
+    return list(zip(Counter(lst).keys(), Counter(lst).values()))  
+
+def show_alldata(data,barcodeList,filterQ,filterL):
+    avg_score_data = []
+    length_data = []
+    for x in data:
+        aList = json.loads(x)
+        if filterQ == 0 and filterL == 0  :
+                avg_score_data.append(aList["Average_Qscore"])
+                length_data.append(aList["Read_length"])   
+        elif int(aList["Average_Qscore"]) >= filterQ  :
+               if filterL == 0  :
+                    avg_score_data.append(aList["Average_Qscore"])
+                    length_data.append(aList["Read_length"])                    
+               if aList["Read_length"] >= filterL :
+                    avg_score_data.append(aList["Average_Qscore"])
+                    length_data.append(aList["Read_length"])            
+    
+    maxQscore_str = str(max_q(avg_score_data))
+    minQscore_str = str(min_q(avg_score_data))
+    meanQscore_str = str(round(mean_q(avg_score_data),2))
+    print("Average_Qscore : Max " + maxQscore_str)
+    print("Average_Qscore : Min " + minQscore_str)
+    print("Average_Qscore : Mean " + meanQscore_str)
+    print("///////////")
+    
+    maxlength = max_q(length_data)
+    maxLength_str = str(f'{maxlength:,.0f}')
+ 
+    minLength_str = str(min_q(length_data))
+    meanLength = round(mean_q(length_data),0)
+    meanLength_str = str(f'{meanLength:,.0f}')
+    
+    print("Average_length : Max " + maxLength_str)
+    print("Average_length : Min " + minLength_str)
+    print("Average_length : Mean " + meanLength_str)
+    
+    len_data = len(data)
+    total_reads = str(f'{len_data:,.0f}')
+    print("Reads : " , total_reads)
+    print("Bases : " + str(sum_q(length_data)))
+
+    plt.scatter(avg_score_data, length_data)
+    plt.title("Distribution of read quality scores")
+    plt.xlabel("Read quality scores")
+    plt.ylabel("Read Length")      
+
+
+    img = BytesIO()      
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    img1_base = base64.b64encode(img.getvalue()).decode('utf8')
+    
+
+ 
+    avg_score_data_list = group_list(avg_score_data)
+    lst = avg_score_data_list
+    lst.sort(key=lambda x:x[0]) 
+    x = []
+    y = []
+    for v in lst:    
+        x.append(v[0])
+        y.append(v[1])
+    plt.plot(x, y)
+    plt.title("Distribution of read quality scores")
+    plt.xlabel("Read quality scores")
+    plt.ylabel("Read density")    
+    img = BytesIO()      
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    img2_base = base64.b64encode(img.getvalue()).decode('utf8')
+    
+
+
+    out_array = np.log10(length_data)
+    length_log = []
+    for v in out_array :    
+        length_log.append(round(v,1))
+    length_data_list = group_list(length_log)
+    lst = length_data_list
+    lst.sort(key=lambda x:x[0])    
+    x = []
+    y = []
+
+    for v in lst: 
+        x.append(v[0])
+        y.append(v[1])
+    plt.plot(x, y)
+    plt.title("Distribution of read length")
+    plt.xlabel("Read length (log scale)")
+    plt.ylabel("Read density")
+    img = BytesIO()      
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    img3_base = base64.b64encode(img.getvalue()).decode('utf8')
+    
+    bcdata = []
+    html2 = ""
+    sum_filter_total_reads = 0
+    filter_status = False
+    for x in barcodeList :
+        bcdata = []
+        for i in data:
+            aaList = json.loads(i)
+            if aaList["Read_barcode"] == x :
+                   bcdata.append(aaList) 
+        print("\\bc//:" + x , len(bcdata))        
+        get_barcodeinfo_result = get_barcodeinfo(bcdata,filterQ,filterL)
+        sum_filter_total_reads += int(get_barcodeinfo_result[0])
+        html2 +=  get_barcodeinfo_result[1]
+        filter_status = get_barcodeinfo_result[2]
+        
+    
+    if (filter_status == True) :
+        total_reads = str(sum_filter_total_reads)
+    html = gen_html("ALL",maxQscore_str,minQscore_str,meanQscore_str,total_reads,maxLength_str,minLength_str,meanLength_str,img1_base,img2_base,img3_base,html2)
+    with open('report.html','w') as f:
+        f.write(html)
 
 def get_barcodeinfo(data,filterQ,filterL):
     avg_score_data = []
     length_data = []
     barcodeName = ""
+    filter_status = False
     for x in data:
         aList = x
         if filterQ == 0 and filterL == 0  :
                 avg_score_data.append(aList["Average_Qscore"])
                 length_data.append(aList["Read_length"])
-                barcodeName = str(aList["Read_barcode"]) 
+                barcodeName = str(aList["Read_barcode"])
+                filter_status = False 
                 
-        if int(aList["Average_Qscore"]) >= filterQ  :
+        elif int(aList["Average_Qscore"]) >= filterQ  :
                if filterL == 0  :
                         avg_score_data.append(aList["Average_Qscore"])
                         length_data.append(aList["Read_length"])
                         barcodeName = str(aList["Read_barcode"])
+                        filter_status = True
+
                     
                if aList["Read_length"] >= filterL :
                         avg_score_data.append(aList["Average_Qscore"])
                         length_data.append(aList["Read_length"])
                         barcodeName = str(aList["Read_barcode"])
+                        filter_status = True
 
     if len(avg_score_data) == 0  |  len(length_data) == 0 :
         return
@@ -57,14 +181,12 @@ def get_barcodeinfo(data,filterQ,filterL):
     maxQscore_str = str(max_q(avg_score_data))
     minQscore_str = str(min_q(avg_score_data))
     meanQscore_str = str(round(mean_q(avg_score_data),2))
-    maxQscore_str = str(max_q(avg_score_data))
-    minQscore_str = str(min_q(avg_score_data))
-    meanQscore_str = str(round(mean_q(avg_score_data),2))
+   
+    print("///////////" +barcodeName)    
     print("Average_Qscore : Max " + maxQscore_str)
     print("Average_Qscore : Min " + minQscore_str)
-    print("Average_Qscore : Mean " + meanQscore_str)
-    print("///////////" +barcodeName)
-    
+    print("Average_Qscore : Mean " + meanQscore_str)    
+  
     maxlength = max_q(length_data)
     maxLength_str = str(f'{maxlength:,.0f}') 
     minLength_str = str(min_q(length_data))
@@ -72,14 +194,18 @@ def get_barcodeinfo(data,filterQ,filterL):
     meanLength_str = str(f'{meanLength:,.0f}')
     print("Average_length : Max " + maxLength_str)
     print("Average_length : Min " + minLength_str)
-    print("Average_length : Mean " + meanLength_str)
-    
-    len_data = len(data)
+    print("Average_length : Mean " + meanLength_str)    
+    print("Filter :",filter_status)
+   
+    if(filter_status==False):
+        len_data = len(data)
+    else:
+        len_data = len(avg_score_data)
     total_reads = str(f'{len_data:,.0f}')
     Bases = str(sum_q(length_data))
     print("Reads : " + total_reads)
     print("Bases : " + Bases)
-    
+    print("//////////////////")
     
     plt.scatter(avg_score_data, length_data)
     plt.title("Distribution of read quality scores")
@@ -127,8 +253,9 @@ def get_barcodeinfo(data,filterQ,filterL):
     plt.savefig(img, format='png')
     plt.close()
     img.seek(0)
-    img3_base = base64.b64encode(img.getvalue()).decode('utf8')  
-    ############################ barcode json return  
+    img3_base = base64.b64encode(img.getvalue()).decode('utf8')
+    
+    result_return = [] 
     # barcode_info = {
     #                   "Average_Qscore_Max": maxQscore_str,
     #                   "Average_Qscore_Min": minQscore_str,
@@ -140,7 +267,7 @@ def get_barcodeinfo(data,filterQ,filterL):
     #                   "Reads": total_reads,
     #                   "Bases": Bases
     #                 }
-    ############################
+    result_return.append(len_data)
     html = ' 		<tr>'
     html += ' 		  <td>' + '<a href="'+barcodeName+'.html" target="_blank">' + barcodeName+'</a></td>'
     html += ' 		  <td>'+total_reads+'</td>'
@@ -151,120 +278,8 @@ def get_barcodeinfo(data,filterQ,filterL):
     html += ' 		  <td>'+minLength_str+'</td>'
     html += ' 		  <td>'+meanLength_str+'</td>'
     html += ' 		</tr>'
-    
-    
+    result_return.append(html)
+    result_return.append(filter_status)
     with open(barcodeName+'.html','w') as f:
         f.write(gen_html(barcodeName,maxQscore_str,minQscore_str,meanQscore_str,total_reads,maxLength_str,minLength_str,meanLength_str,img1_base,img2_base,img3_base,""))
-    return html
-    
-def show_alldata(data,barcodeList,filterQ,filterL):
-    avg_score_data = []
-    length_data = []
-    for x in data:
-        aList = json.loads(x)
-        if filterQ == 0 and filterL == 0  :
-                avg_score_data.append(aList["Average_Qscore"])
-                length_data.append(aList["Read_length"])   
-        if int(aList["Average_Qscore"]) >= filterQ  :
-               if filterL == 0  :
-                    avg_score_data.append(aList["Average_Qscore"])
-                    length_data.append(aList["Read_length"])
-                    
-               if aList["Read_length"] >= filterL :
-                    avg_score_data.append(aList["Average_Qscore"])
-                    length_data.append(aList["Read_length"])             
-        #df = pd.DataFrame(aList, columns=list("ABCD"))
-       # print(aList["Average_Qscore"])
-    maxQscore_str = str(max_q(avg_score_data))
-    minQscore_str = str(min_q(avg_score_data))
-    meanQscore_str = str(round(mean_q(avg_score_data),2))
-    print("Average_Qscore : Max " + maxQscore_str)
-    print("Average_Qscore : Min " + minQscore_str)
-    print("Average_Qscore : Mean " + meanQscore_str)
-    print("///////////")
-    
-    maxlength = max_q(length_data)
-    maxLength_str = str(f'{maxlength:,.0f}')
- 
-    minLength_str = str(min_q(length_data))
-    meanLength = round(mean_q(length_data),0)
-    meanLength_str = str(f'{meanLength:,.0f}')
-    print("Average_length : Max " + maxLength_str)
-    print("Average_length : Min " + minLength_str)
-    print("Average_length : Mean " + meanLength_str)
-    
-    len_data = len(data)
-    total_reads = str(f'{len_data:,.0f}')
-    print("Reads : " , len_data)
-    print("Bases : " + str(sum_q(length_data)))
-
-    plt.scatter(avg_score_data, length_data)
-    plt.title("Distribution of read quality scores")
-    plt.xlabel("Read quality scores")
-    plt.ylabel("Read Length")      
-
-
-    img = BytesIO()      
-    plt.savefig(img, format='png')
-    plt.close()
-    img.seek(0)
-    img1_base = base64.b64encode(img.getvalue()).decode('utf8')
-    
-    out_array = np.log10(length_data)
-    length_log = []
-    for v in out_array :    
-        length_log.append(round(v,1))
- 
-    avg_score_data_list = group_list(avg_score_data)
-    lst = avg_score_data_list
-    lst.sort(key=lambda x:x[0]) 
-    x = []
-    y = []
-    for v in lst:    
-        x.append(v[0])
-        y.append(v[1])
-
-    plt.plot(x, y)
-    plt.title("Distribution of read quality scores")
-    plt.xlabel("Read quality scores")
-    plt.ylabel("Read density")    
-    img = BytesIO()      
-    plt.savefig(img, format='png')
-    plt.close()
-    img.seek(0)
-    img2_base = base64.b64encode(img.getvalue()).decode('utf8')
-    
-    length_data_list = group_list(length_log)
-    lst = length_data_list
-    lst.sort(key=lambda x:x[0])    
-    x = []
-    y = []
-
-    for v in lst: 
-        x.append(v[0])
-        y.append(v[1])
-    plt.plot(x, y)
-    plt.title("Distribution of read length")
-    plt.xlabel("Read length (log scale)")
-    plt.ylabel("Read density")
-    img = BytesIO()      
-    plt.savefig(img, format='png')
-    plt.close()
-    img.seek(0)
-    img3_base = base64.b64encode(img.getvalue()).decode('utf8')
-    
-    bcdata = []
-    html2 = ""
-    for x in barcodeList :
-        bcdata = []
-        for i in data:
-            aaList = json.loads(i)
-            if aaList["Read_barcode"] == x :
-                   bcdata.append(aaList) 
-        print("\\bc//:" + x , len(bcdata))        
-        html2 += get_barcodeinfo(bcdata,filterQ,filterL)
-    
-    
-    html = gen_html("ALL",maxQscore_str,minQscore_str,meanQscore_str,total_reads,maxLength_str,minLength_str,meanLength_str,img1_base,img2_base,img3_base,html2)
-    with open('report.html','w') as f:
-        f.write(html)
+    return result_return
